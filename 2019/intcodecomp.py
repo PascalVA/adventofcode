bc8948b47913ea4f83c4c5bce084c65bd675323d
Pascal Van Acker <vanacker.pascal@outlook.com>
2019-12-22 14:46:41 +0100

    update intcode computer + add unit tests

--- 2019/intcodecomp.py
+++ 2019/intcodecomp.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python
 from operator import add, mul
+from array import array
+
+
+class IntcodeMem(object):
+
+    def __init__(self, memory):
+        self.mem = {}
+        for i, item in enumerate(memory):
+            self.mem[i] = item
+
+    def __getitem__(self, index):
+        return self.mem.get(index, 0)
+
+    def __setitem__(self, index, value):
+        self.mem[index] = value
 
 
 class IntcodeComp(object):
 
-    def __init__(self, inputs, intcodes):
+    def __init__(self, inputs, program):
         self.inputs = inputs or []
-        self.mem = intcodes[:]  # copy
+
+        self.mem = IntcodeMem(program)
 
         self.op = {
             1: self.add,
@@ -16,10 +32,12 @@ class IntcodeComp(object):
             5: self.jump_true,
             6: self.jump_false,
             7: self.lt,
-            8: self.eq
+            8: self.eq,
+            9: self.rb_offset
         }
 
         self.ptr = 0
+        self.rb = 0  # relative base
         self.opcode = 0
         self.modes = [0, 0, 0]
 
@@ -32,7 +50,19 @@ class IntcodeComp(object):
         return params
 
     def _parse_param(self, v, mode):
-        return self.mem[v] if not mode else v
+        return {
+          0: self.mem[v],           # position mode
+          1: v,                     # immediate mode
+          2: self.mem[v + self.rb]  # relative mode
+        }[mode]
+
+    def _parse_write_param(self, v, mode):
+        if mode == 1:
+            raise ValueError("Write params can never have immediate mode (1)")
+        return {
+          0: self.mem[v],           # position mode
+          2: self.mem[v] + self.rb  # relative mode
+        }[mode]
 
     def _jmp(self, func):
         params = self._get_params(2)
@@ -44,20 +74,20 @@ class IntcodeComp(object):
 
     def add(self):
         params = self._get_params(2)
-        addr = self.mem[self.ptr + 3]
+        addr = self._parse_write_param(self.ptr + 3, self.modes[2])
         self.mem[addr] = add(*params[:2])
         self.ptr += 4
 
     def mul(self):
         params = self._get_params(2)
-        addr = self.mem[self.ptr + 3]
+        addr = self._parse_write_param(self.ptr + 3, self.modes[2])
         self.mem[addr] = mul(*params[:2])
         self.ptr += 4
 
     def write(self):
         if len(self.inputs) == 0:
             return False
-        addr = self.mem[self.ptr + 1]
+        addr = self._parse_write_param(self.ptr + 1, self.modes[0])
         self.mem[addr] = self.inputs.pop(0)
         self.ptr += 2
 
@@ -75,15 +105,19 @@ class IntcodeComp(object):
 
     def lt(self):
         params = self._get_params(2)
-        addr = self.mem[self.ptr + 3]
+        addr = self._parse_write_param(self.ptr + 3, self.modes[2])
         self.mem[addr] = int(params[0] < params[1])
         self.ptr += 4
 
     def eq(self):
         params = self._get_params(2)
-        addr = self.mem[self.ptr + 3]
+        addr = self._parse_write_param(self.ptr + 3, self.modes[2])
         self.mem[addr] = int(params[0] == params[1])
         self.ptr += 4
+
+    def rb_offset(self):
+        self.rb += self._parse_param(self.mem[self.ptr + 1], self.modes[0])
+        self.ptr += 2
 
     def _parse_instruction(self):
         inst = "{:05}".format(self.mem[self.ptr])
@@ -97,7 +131,13 @@ class IntcodeComp(object):
         while True:
             self._parse_instruction()
 
+            #print("mem: %s\nptr: %d, op: %d, modes: %s, rb: %s"
+            #       % (self.mem.mem.values(), self.ptr, self.opcode, self.modes, self.rb))
+            #from time import sleep
+            #sleep(0.5)
+
             if self.opcode == 99:
+                self.ptr = 0
                 return True
 
             res = self.op[self.opcode]()
@@ -105,75 +145,3 @@ class IntcodeComp(object):
                 break
             if isinstance(res, int):
                 return res
-
-            if self.ptr >= len(self.mem):
-                break
-
-tests = [
-    {
-        "mem": [3,9,8,9,10,9,4,9,99,-1,8],  # inp eq 8 position mode
-        "inp": [8],
-        "res": 1
-    },
-    {
-        "mem": [3,9,8,9,10,9,4,9,99,-1,8],  # inp eq 8 position mode
-        "inp": [7],
-        "res": 0
-    },
-    {
-        "mem": [3,9,8,9,10,9,4,9,99,-1,8],  # inp eq 8 position mode
-        "inp": [9],
-        "res": 0
-    },
-    {
-        "mem": [3,9,7,9,10,9,4,9,99,-1,8],  # inp lt 8 position mode
-        "inp": [9],
-        "res": 0
-    },
-    {
-        "mem": [3,9,7,9,10,9,4,9,99,-1,8],  # inp lt 8 position mode
-        "inp": [8],
-        "res": 0
-    },
-    {
-        "mem": [3,9,7,9,10,9,4,9,99,-1,8],  # inp lt 8 position mode
-        "inp": [7],
-        "res": 1
-    },
-    {
-        "mem": [3,3,1108,-1,8,3,4,3,99],    # inp eq 8 immediate mode
-        "inp": [9],
-        "res": 0
-    },
-    {
-        "mem": [3,3,1108,-1,8,3,4,3,99],    # inp eq 8 immediate mode
-        "inp": [8],
-        "res": 1
-    },
-    {
-        "mem": [3,3,1108,-1,8,3,4,3,99],    # inp eq 8 immediate mode
-        "inp": [7],
-        "res": 0
-    },
-    {
-        "mem": [3,3,1107,-1,8,3,4,3,99],    # lt 8 immediate mode
-        "inp": [9],
-        "res": 0
-    },
-    {
-        "mem": [3,3,1107,-1,8,3,4,3,99],    # lt 8 immediate mode
-        "inp": [8],
-        "res": 0
-    },
-    {
-        "mem": [3,3,1107,-1,8,3,4,3,99],    # lt 8 immediate mode
-        "inp": [7],
-        "res": 1
-    }
-]
-
-if __name__ == '__main__':
-    for test in tests:
-        res = IntcodeComp(test["inp"], test["mem"]).run()
-        if not(res == test["res"]):
-            print("Program %s failed with output: %s, expected: %s" % (test["mem"], res, test["res"]))
